c10fe314830b2c14524d926eb012fe6da4488e7d
Anders Christiansen Sørby <user8715@gmail.com>
2018-11-01 09:34:39 +0100

    Add some tests

--- src/matmod.jl
+++ src/matmod.jl
@@ -1,9 +1,28 @@
 module matmod
-using Plots, DifferentialEquations
+    using Plots, DifferentialEquations, ProgressMeter
 
-f(u,p,t) = 1.01*u
-u0=1/2
-tspan = (0.0,1.0)
-prob = ODEProblem(f,u0,tspan)
+    f(u,p,t) = 1.01*u
+    u0=1/2
+    tspan = (0.0,1.0)
+    prob = ODEProblem(f,u0,tspan)
+    solution = solve(prob,Tsit5(),reltol=1e-2,abstol=1e-2)
 
+    plot(sol)
+
+    mutable struct Glacier
+        t
+        h
+        u
+        v
+    end
+
+    function step!(g::Glacier)
+
+    end
+
+    g = Glacier()
+
+    @showprogress for i in 1:steps
+        step!(g)
+    end
 end # module
